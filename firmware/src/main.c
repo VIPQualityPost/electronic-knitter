aed32d6c5452d49e3f94ad4bda9f7280dbd0953f
matei jordache <mateijordache@live.com>
2023-04-24 14:40:05 -0400

    add logic analyzer config

--- firmware/src/main.c
+++ firmware/src/main.c
@@ -1,20 +1,20 @@
 /* USER CODE BEGIN Header */
 /**
-  ******************************************************************************
-  * @file           : main.c
-  * @brief          : Main program body
-  ******************************************************************************
-  * @attention
-  *
-  * Copyright (c) 2023 STMicroelectronics.
-  * All rights reserved.
-  *
-  * This software is licensed under terms that can be found in the LICENSE file
-  * in the root directory of this software component.
-  * If no LICENSE file comes with this software, it is provided AS-IS.
-  *
-  ******************************************************************************
-  */
+ ******************************************************************************
+ * @file           : main.c
+ * @brief          : Main program body
+ ******************************************************************************
+ * @attention
+ *
+ * Copyright (c) 2023 STMicroelectronics.
+ * All rights reserved.
+ *
+ * This software is licensed under terms that can be found in the LICENSE file
+ * in the root directory of this software component.
+ * If no LICENSE file comes with this software, it is provided AS-IS.
+ *
+ ******************************************************************************
+ */
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
@@ -40,6 +40,15 @@
 /* Private define ------------------------------------------------------------*/
 /* USER CODE BEGIN PD */
 
+#define machineType 930
+#define carriageType "K"
+#define offsetLeft
+#define offsetRight
+#define EOL_L_min
+#define EOL_L_max
+#define EOL_R_min
+#define EOL_R_max
+
 /* USER CODE END PD */
 
 /* Private macro -------------------------------------------------------------*/
@@ -50,6 +59,12 @@
 /* Private variables ---------------------------------------------------------*/
 
 /* USER CODE BEGIN PV */
+
+uint16_t 	EOL_result[2]; 		// ADC DMA buffer, left [0] right [1]
+uint16_t 	currentRow;			// current row of the design. Only increment after full row (trace retrace)
+uint8_t 	*rowPattern;		// whole row is stored as 25 bytes (200 bits)
+uint16_t 	currentPattern;		// GPIO_PORT_B state (entire bank)
+uint8_t 	currentPos;			// 0-200 current location, center of carriage
 
 /* USER CODE END PV */
 
@@ -62,199 +77,203 @@ void SystemClock_Config(void);
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
 
-// ADC DMA buffer, 12 bit. left [0] right [1]
-uint16_t  EOL_result[2];
-
-// 200 bits (one for each needle) arranged 25 sequential bytes
-// we will load the pattern by byte
-uint8_t  *rowPattern;
-uint16_t selectorPattern;
-
-// representing current carriage position 0-200 (256)
-uint8_t   currentPos;
-
 /* USER CODE END 0 */
 
 /**
-  * @brief  The application entry point.
-  * @retval int
-  */
+ * @brief  The application entry point.
+ * @retval int
+ */
 int main(void)
 {
-  /* USER CODE BEGIN 1 */
+	/* USER CODE BEGIN 1 */
 
-  // 200 bits (25 bytes)
-  pattern = calloc(25, 8);
-  /* USER CODE END 1 */
+	// 200 bits (25 bytes)
+	rowPattern = calloc(25, 8);
+	/* USER CODE END 1 */
 
-  /* MCU Configuration--------------------------------------------------------*/
+	/* MCU Configuration--------------------------------------------------------*/
 
-  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
-  HAL_Init();
+	/* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+	HAL_Init();
 
-  /* USER CODE BEGIN Init */
+	/* USER CODE BEGIN Init */
 
-  /* USER CODE END Init */
+	/* USER CODE END Init */
 
-  /* Configure the system clock */
-  SystemClock_Config();
+	/* Configure the system clock */
+	SystemClock_Config();
 
-  /* USER CODE BEGIN SysInit */
+	/* USER CODE BEGIN SysInit */
 
-  /* USER CODE END SysInit */
+	/* USER CODE END SysInit */
 
-  /* Initialize all configured peripherals */
-  MX_GPIO_Init();
-  MX_DMA_Init();
-  MX_SPI1_Init();
-  MX_TIM1_Init();
-  MX_USB_DEVICE_Init();
-  MX_USART2_UART_Init();
-  MX_ADC1_Init();
-  MX_TIM2_Init();
-  /* USER CODE BEGIN 2 */
+	/* Initialize all configured peripherals */
+	MX_GPIO_Init();
+	MX_DMA_Init();
+	MX_SPI1_Init();
+	MX_TIM1_Init();
+	MX_USB_DEVICE_Init();
+	MX_USART2_UART_Init();
+	MX_ADC1_Init();
+	MX_TIM2_Init();
+	/* USER CODE BEGIN 2 */
 
-  // start the encoder interface
-  HAL_TIM_Encoder_Start(&htim1,TIM_CHANNEL_ALL);
+	// start the encoder interface
+	HAL_TIM_Encoder_Start(&htim1, TIM_CHANNEL_ALL);
 
-  // calibrate the ADC and DMA scan
-  HAL_ADCEx_Calibration_Start(&hadc1);
+	// calibrate the ADC and DMA scan
+	HAL_ADCEx_Calibration_Start(&hadc1);
 
-  // start the PWM for heartbeat 
-  HAL_TIM_PWM_Start(&htim2, TIM_CHANNEL_ALL);
-  
-  /* USER CODE END 2 */
+	// start the PWM for heartbeat
+	HAL_TIM_PWM_Start(&htim2, TIM_CHANNEL_ALL);
 
-  /* Infinite loop */
-  /* USER CODE BEGIN WHILE */
-  while (1)
-  {
-    /* USER CODE END WHILE */
+	/* USER CODE END 2 */
 
-    // start a read
-    HAL_ADC_Start_DMA(&hadc1, EOL_result, 2);
+	/* Infinite loop */
+	/* USER CODE BEGIN WHILE */
+	while (1)
+	{
+		/* USER CODE END WHILE */
 
-    //
+		// start a read
+		HAL_ADC_Start_DMA(&hadc1, EOL_result, 2);
 
-    /* USER CODE BEGIN 3 */
-  }
-  /* USER CODE END 3 */
+		//
+
+		/* USER CODE BEGIN 3 */
+	}
+	/* USER CODE END 3 */
 }
 
 /**
-  * @brief System Clock Configuration
-  * @retval None
-  */
+ * @brief System Clock Configuration
+ * @retval None
+ */
 void SystemClock_Config(void)
 {
-  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
-  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
-  RCC_PeriphCLKInitTypeDef PeriphClkInit = {0};
+	RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+	RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+	RCC_PeriphCLKInitTypeDef PeriphClkInit = {0};
 
-  /** Initializes the RCC Oscillators according to the specified parameters
-  * in the RCC_OscInitTypeDef structure.
-  */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
-  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
-  RCC_OscInitStruct.HSEPredivValue = RCC_HSE_PREDIV_DIV1;
-  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
-  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
-  RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL6;
-  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
-  {
-    Error_Handler();
-  }
+	/** Initializes the RCC Oscillators according to the specified parameters
+	 * in the RCC_OscInitTypeDef structure.
+	 */
+	RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+	RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+	RCC_OscInitStruct.HSEPredivValue = RCC_HSE_PREDIV_DIV1;
+	RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+	RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+	RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+	RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL6;
+	if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+	{
+		Error_Handler();
+	}
 
-  /** Initializes the CPU, AHB and APB buses clocks
-  */
-  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
-  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
-  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
-  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+	/** Initializes the CPU, AHB and APB buses clocks
+	 */
+	RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
+	RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+	RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+	RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+	RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
 
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_ADC|RCC_PERIPHCLK_USB;
-  PeriphClkInit.AdcClockSelection = RCC_ADCPCLK2_DIV6;
-  PeriphClkInit.UsbClockSelection = RCC_USBCLKSOURCE_PLL_DIV1_5;
-  if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK)
-  {
-    Error_Handler();
-  }
+	if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
+	{
+		Error_Handler();
+	}
+	PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_ADC | RCC_PERIPHCLK_USB;
+	PeriphClkInit.AdcClockSelection = RCC_ADCPCLK2_DIV6;
+	PeriphClkInit.UsbClockSelection = RCC_USBCLKSOURCE_PLL_DIV1_5;
+	if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK)
+	{
+		Error_Handler();
+	}
 }
 
 /* USER CODE BEGIN 4 */
 
 // ADC DMA callback after conversion done
-void HAL_ADC_ConvCpltCallback(ADC_HandleTypeDef* hadc1){
-  // After ADC DMA, check if the stored values exceed the threshold
+void HAL_ADC_ConvCpltCallback(ADC_HandleTypeDef *hadc1)
+{
+	// After ADC DMA, check if the stored values exceed the threshold
 
-  if(EOL_result[0] > 0xFFFF/2){
-    TIM1->CNT = 0;
-  }
-  else if(EOL_result[1] > 0xFFFF/2){
-    TIM1->CNT = 200;
-  }
-}
-
-// blink the little LED.
-void heartbeat(void){
-  uint8_t i=0;
-  HAL_GPIO_TogglePin(HEARTBEAT_GPIO_Port, HEARTBEAT_Pin);
-  HAL_Delay(200);
-
-}
-
-// using the current needle position, find out what solenoid to activate.
-uint8_t getSolenoid(uint8_t needleNum){
-  /** if we agree that the leftmost needle is 0, rightmost 200
-    * then we can just mask the lower nibble of needleNum to
-    * return the solenoid number.
-    */
-  return needleNum & 0x0F;
+	if (EOL_result[0] > 0xFFFF / 2)
+	{
+		TIM1->CNT = 0;
+	}
+	else if (EOL_result[1] > 0xFFFF / 2)
+	{
+		TIM1->CNT = 200;
+	}
 }
 
 // using the current needle position, figure out what byte of pattern we are in
-uint8_t findPatternByte(uint8_t needleNum){
-  needleNum = ceil(needleNum/8);
-  return (uint8_t*)needleNum;
+uint8_t findPatternByte(uint8_t needleNum)
+{	
+	double patternByte = needleNum;
+	patternByte = ceil(needleNum / 8);
+	return (uint8_t *)needleNum;
+}
+
+// using the current needle position, find out what solenoid to activate.
+uint8_t getSolenoid(uint8_t needleNum)
+{
+	/** if we agree that the leftmost needle is 0, rightmost 200
+	 * then we can just mask the lower nibble of needleNum to
+	 * return the solenoid number.
+	 */
+	return needleNum & 0x0F;
+}
+
+// Set the solenoids, entire bank at once.
+void setSolenoids(uint16_t solenoidPattern){
+	uint16_t currentPattern = solenoidPattern;
+
+	// need to byteswap because I laid out the board wrong.
+	// well, my layout makes more sense but it does not agree with the Brother pinout.
+	solenoidPattern = ((solenoidPattern << 8) & 0xFF00) | ((solenoidPattern >> 8) & 0x00FF);
+
+	// the bus is 16 bits wide, one for each solenoid, hooked up on the GPIOB port. 
+	// we can just write a word here to address the whole port at once.
+	GPIOB->ODR = solenoidPattern;
+}
+
+// Turn off all the solenoids.
+void disableSolenoids(void){
+	GPIOB->ODR = 0x0000;
 }
 
 /* USER CODE END 4 */
 
 /**
-  * @brief  This function is executed in case of error occurrence.
-  * @retval None
-  */
+ * @brief  This function is executed in case of error occurrence.
+ * @retval None
+ */
 void Error_Handler(void)
 {
-  /* USER CODE BEGIN Error_Handler_Debug */
-  /* User can add his own implementation to report the HAL error return state */
-  __disable_irq();
-  while (1)
-  {
-  }
-  /* USER CODE END Error_Handler_Debug */
+	/* USER CODE BEGIN Error_Handler_Debug */
+	/* User can add his own implementation to report the HAL error return state */
+	__disable_irq();
+	while (1)
+	{
+	}
+	/* USER CODE END Error_Handler_Debug */
 }
 
-#ifdef  USE_FULL_ASSERT
+#ifdef USE_FULL_ASSERT
 /**
-  * @brief  Reports the name of the source file and the source line number
-  *         where the assert_param error has occurred.
-  * @param  file: pointer to the source file name
-  * @param  line: assert_param error line source number
-  * @retval None
-  */
+ * @brief  Reports the name of the source file and the source line number
+ *         where the assert_param error has occurred.
+ * @param  file: pointer to the source file name
+ * @param  line: assert_param error line source number
+ * @retval None
+ */
 void assert_failed(uint8_t *file, uint32_t line)
 {
-  /* USER CODE BEGIN 6 */
-  /* User can add his own implementation to report the file name and line number,
-     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
-  /* USER CODE END 6 */
+	/* USER CODE BEGIN 6 */
+	/* User can add his own implementation to report the file name and line number,
+	   ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+	/* USER CODE END 6 */
 }
 #endif /* USE_FULL_ASSERT */
